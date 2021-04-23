7bbf9dfaf6f5797a5f0acaab25d0bed5c86c245e
Sushant Mahadik <sushant.mahadik@tadigital.com>
2021-04-23 15:19:26 +0530

    MVC and Controller Added

--- Demo/Demo/Startup.cs
+++ Demo/Demo/Startup.cs
@@ -16,6 +16,7 @@ namespace Demo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,12 +29,18 @@ namespace Demo
 
             app.UseRouting();
 
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
+
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
